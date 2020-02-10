e7661ec87cfd11c07be4bc4c9e46f4940088a263
Drew Gochenaur <drew@raincity.io>
2020-02-09 21:23:26 -0800

    exit on forward failure

--- tunnel.sh
+++ tunnel.sh
@@ -15,7 +15,10 @@ if [ -z $remote_port ]; then
 fi
 
 mkdir -p $(dirname $log)
+# in a while loop because autossh will exit
+# if dns fails, or if it can not forward the
+# port, basically, if ssh does start initally
 while true; do
-    autossh -M 0 -i ${HOME}/.ssh/id_rsa_home -N -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R ${remote_port}:localhost:22 remote@kronos.raincity.io >>$log 2>&1
+    autossh -M 0 -i ${HOME}/.ssh/id_rsa_home -N -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "ExitOnForwardFailure=yes" -R ${remote_port}:localhost:22 remote@kronos.raincity.io >>$log 2>&1
     sleep 1
 done
